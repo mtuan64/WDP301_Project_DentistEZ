3410d3216f55cf221b1c880e92976e10c9de7008
Huyennthe <huyennthe172781@fpt.edu.vn>
2025-06-01 00:06:42 +0700

    update

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+dotenv.config();
 
 const authMiddleware = (req, res, next) => {
   // Extract token from Authorization header
@@ -18,5 +20,119 @@ const authMiddleware = (req, res, next) => {
     res.status(401).json({ msg: 'Token không hợp lệ' });
   }
 };
+const authAdminMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      message: "Không tìm thấy token hoặc token không hợp lệ",
+      status: "ERROR",
+    });
+  }
+  const token = authHeader.split(" ")[1];
 
-module.exports = authMiddleware;
+  jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).json({
+        message: "Token không hợp lệ hoặc đã hết hạn",
+        status: "ERROR",
+      });
+    }
+   
+    if (decoded.role === "admin") {
+      
+      req.user = decoded;
+      next();
+    } else {
+      return res.status(403).json({
+        message: "Bạn không có quyền truy cập (chỉ dành cho ADMIN)",
+        status: "ERROR",
+      });
+    }
+  });
+};
+const authDentistMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      message: "Không tìm thấy token hoặc token không hợp lệ",
+      status: "ERROR",
+    });
+  }
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).json({
+        message: "Token không hợp lệ hoặc đã hết hạn",
+        status: "ERROR",
+      });
+    }
+    if (decoded.role === "DENTIST") {
+      req.user = decoded;
+      next();
+    } else {
+      return res.status(403).json({
+        message: "Bạn không có quyền truy cập (chỉ dành cho DENTIST)",
+        status: "ERROR",
+      });
+    }
+  });
+};
+const authPatientMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      message: "Không tìm thấy token hoặc token không hợp lệ",
+      status: "ERROR",
+    });
+  }
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).json({
+        message: "Token không hợp lệ hoặc đã hết hạn",
+        status: "ERROR",
+      });
+    }
+    if (decoded.role === "patient") {
+      req.user = decoded;
+      next();
+    } else {
+      return res.status(403).json({
+        message: "Bạn không có quyền truy cập (chỉ dành cho PATIENT)",
+        status: "ERROR",
+      });
+    }
+  });
+};
+const authStaffMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({
+      message: "Không tìm thấy token hoặc token không hợp lệ",
+      status: "ERROR",
+    });
+  }
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).json({
+        message: "Token không hợp lệ hoặc đã hết hạn",
+        status: "ERROR",
+      });
+    }
+    if (decoded.role === "staff") {
+      req.user = decoded;
+      next();
+    } else {
+      return res.status(403).json({
+        message: "Bạn không có quyền truy cập (chỉ dành cho STAFF)",
+        status: "ERROR",
+      });
+    }
+  });
+};
+
+module.exports = { authMiddleware, authAdminMiddleware, authDentistMiddleware, authPatientMiddleware, authStaffMiddleware }; 
