10c7a9582fec7e6c91b4af73bce9d553af59ee4b
Huyennthe <huyennthe172781@fpt.edu.vn>
2025-06-02 10:35:59 +0700

    update

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middleware/authMiddleware');
+const {authMiddleware} = require('../middleware/authMiddleware');
 const { registerUser, loginUser, uploadProfilePicture, updateUser, upload } = require('../controllers/authController');
 const {getAllDoctors, getDoctorById} = require('../controllers/doctorController');
 
